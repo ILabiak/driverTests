7d899c064bcd1fc566ac002f6d007bc9109f7dc5
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-07-21 12:21:23 +0300

    create useAuthData it will reduce usage of the same code in each component

--- client/src/components/Sections.jsx
+++ client/src/components/Sections.jsx
@@ -1,87 +1,28 @@
 import './sections.css';
 import Layout from './Layout';
 import Login from './Login'
+import useAuthData from './useAuthData';
 import React, { useState, useEffect, useRef } from 'react';
 import { useCookies } from 'react-cookie';
 
 function Section() {
-    const [showLoginForm, setShowLoginForm] = useState(false);
-    const [showDropdown, setShowDropdown] = useState(false);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [userEmail, setUserEmail] = useState("");
     const [cookies] = useCookies();
-    const loginContainerRef = useRef(null);
+    const {
+        showLoginForm,
+        setShowLoginForm,
+        showDropdown,
+        setShowDropdown,
+        isAuthenticated,
+        setIsAuthenticated,
+        userEmail,
+        setUserEmail,
+        loginContainerRef,
+        handleLoginLinkClick,
+        handleProfileIconClick,
+        handleLogout,
+      } = useAuthData();
 
-    const handleLoginLinkClick = () => {
-        setShowLoginForm(true);
-    };
 
-    const handleProfileIconClick = () => {
-        setShowDropdown(!showDropdown);
-    }
-
-    const handleLogout = () => {
-        document.cookie = 'sessionID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-        window.location.reload(false);
-    }
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:3005/usermail', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ session_id: cookies.sessionID }),
-                    credentials: 'include'
-                });
-                if (response.status === 200) {
-                    const data = await response.json();
-                    setUserEmail(data.email || '')
-                } else if (response.status === 401) {
-                    //delete cookie, reload page
-                    document.cookie = 'sessionID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-                    window.location.reload(false);
-                } else {
-                    console.log('Some other error');
-                }
-            } catch (error) {
-                console.log('Error while getting user data', error);
-            }
-        }
-
-        if (cookies.sessionID) {
-            setIsAuthenticated(true);
-            fetchData().catch(console.error)
-        } else {
-            setIsAuthenticated(false);
-        }
-    }, [cookies]);
-
-    useEffect(() => {
-        function handleClickOutside(event) {
-            if (
-                loginContainerRef.current &&
-                !loginContainerRef.current.contains(event.target)
-            ) {
-                setShowLoginForm(false);
-            }
-        }
-
-        function handleEscapeKey(event) {
-            if (event.keyCode === 27) {
-                setShowLoginForm(false);
-            }
-        }
-
-        document.addEventListener('mousedown', handleClickOutside);
-        document.addEventListener('keydown', handleEscapeKey);
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-            document.removeEventListener('keydown', handleEscapeKey);
-        };
-    }, []);
 
     return (
         <div className={'App' + (showLoginForm ? ' active' : '')}>
---
+++ client/src/components/useAuthData.jsx
@@ -0,0 +1,99 @@
+import { useState, useEffect, useRef } from 'react';
+import { useCookies } from 'react-cookie';
+
+const useAuthData = () => {
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+  const [cookies] = useCookies();
+  const loginContainerRef = useRef(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3005/usermail', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ session_id: cookies.sessionID }),
+          credentials: 'include'
+        });
+        if (response.status === 200) {
+          const data = await response.json();
+          setUserEmail(data.email || '')
+        } else if (response.status === 401) {
+          //delete cookie, reload page
+          document.cookie = 'sessionID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+          window.location.reload(false);
+        } else {
+          console.log('Some other error');
+        }
+      } catch (error) {
+        console.log('Error while getting user data', error);
+      }
+    }
+
+    if (cookies.sessionID) {
+      setIsAuthenticated(true);
+      fetchData().catch(console.error)
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, [cookies]);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        loginContainerRef.current &&
+        !loginContainerRef.current.contains(event.target)
+      ) {
+        setShowLoginForm(false);
+      }
+    }
+
+    function handleEscapeKey(event) {
+      if (event.keyCode === 27) {
+        setShowLoginForm(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('keydown', handleEscapeKey);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('keydown', handleEscapeKey);
+    };
+  }, []);
+
+  const handleLoginLinkClick = () => {
+    setShowLoginForm(true);
+  };
+
+  const handleProfileIconClick = () => {
+    setShowDropdown(!showDropdown);
+  }
+
+  const handleLogout = () => {
+    document.cookie = 'sessionID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    window.location.reload(false);
+  }
+
+  return {
+    showLoginForm,
+    setShowLoginForm,
+    showDropdown,
+    setShowDropdown,
+    isAuthenticated,
+    setIsAuthenticated,
+    userEmail,
+    setUserEmail,
+    loginContainerRef,
+    handleLoginLinkClick,
+    handleProfileIconClick,
+    handleLogout,
+  };
+}
+
+export default useAuthData;
