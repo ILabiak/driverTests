52a7e51cdd52fb950dee395060d82079b1d665de
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-07-23 12:36:54 +0300

    add menuHeader

--- client/src/components/Sections.jsx
+++ client/src/components/Sections.jsx
@@ -33,7 +33,28 @@ function Sections() {
                 </div>
             )
             }
-            <div style={{height: "100vh", width: "100vw", backgroundColor:"#fff"}}></div>
+            <div className='container sectionspage'>
+                <div className='menuHeader'>
+                    <ul className='menuheader container'>
+                        <li className='singleton current-menu-li'>
+                        <a href="/sections" class="sections">Питання до теми</a>
+                        </li>
+                        <li className='singleton'>
+                        <a href="/twenty-questions" class="twenty-questions">20 випадкових питань</a>
+                        </li>
+                        <li className='singleton'>
+                        <a href="/favourites" class="favourites">Обрані питання</a>
+                        </li>
+                        <li className='singleton'>
+                        <a href="/exam" class="exam">Іспит</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className='sectionRows'>
+
+                </div>
+            </div>
+            {/* <div style={{height: "100vh", width: "100vw", backgroundColor:"#fff"}}></div> */}
         </div>
         
 
