a221231934c9aad4e8a82eecc5fe133da96e0ae7
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-07-24 16:49:55 +0300

    small fixes in behaviour of a element

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -16,52 +16,55 @@ function Layout(props) {
 
   return (
     <header className='App-header'>
-    <div className='headerContainer'>
-      <a href='/'>
-        <img src={logo} className='App-logo' alt='logo' />
-      </a>
-      <ul className='headerList'>
-        <li>
-          <a href='/sections'>Тести з ПДР</a>
-        </li>
-        <li>
-          <a href='/exam'>Іспит з водіння</a>
-        </li>
-        <li className={showDropdown ? 'headerProfileActive' : ''}>
-          {isAuthenticated ? (
-            <div className="profileContainer">
-              <div className="action" onClick={handleProfileIconClick}>
-                <div className="profile">
-                  <img src={profileIcon} alt="profile-img" />
-                </div>
-                <div className={`menu ${showDropdown ? 'active' : ''}`}>
-                  <h3>
-                    {userEmail}<br />
-                    <span>Звичайний користувач</span>
-                  </h3>
-                  <ul>
-                    <li>
-                      <a href="#">Мій профіль</a>
-                    </li>
-                    <li>
-                      <a href="/sections">Teсти</a>
-                    </li>
-                    <li>
-                      <i className="far fa-envelope"></i>
-                      <a onClick={handleLogout}>Вийти</a>
-                    </li>
-                  </ul>
+      <div className='headerContainer'>
+        <a href='/'>
+          <img src={logo} className='App-logo' alt='logo' />
+        </a>
+        <ul className='headerList'>
+          <li>
+            <a href='/sections'>Тести з ПДР</a>
+          </li>
+          <li>
+            <a href='/exam'>Іспит з водіння</a>
+          </li>
+          <li className={showDropdown ? 'headerProfileActive' : ''}>
+            {isAuthenticated ? (
+              <div className="profileContainer">
+                <div className="action" onClick={handleProfileIconClick}>
+                  <div className="profile">
+                    <img src={profileIcon} alt="profile-img" />
+                  </div>
+                  <div className={`menu ${showDropdown ? 'active' : ''}`}>
+                    <h3>
+                      {userEmail}<br />
+                      <span>Звичайний користувач</span>
+                    </h3>
+                    <ul>
+                      <li>
+                        <a href="#">Мій профіль</a>
+                      </li>
+                      <li>
+                        <a href="/sections">Teсти</a>
+                      </li>
+                      <li>
+                        <i className="far fa-envelope"></i>
+                        <a onClick={handleLogout}>Вийти</a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-          ) : (
-            <a onClick={handleLoginLinkClick}>
-              Особистий кабінет
-            </a>
-          )}
-        </li>
-      </ul>
-    </div>
+            ) : (
+              <li>
+                <a onClick={handleLoginLinkClick}>
+                  Особистий кабінет
+                </a>
+              </li>
+
+            )}
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import './main.css';
 import Login from './Login';
 import Layout from './Layout';
