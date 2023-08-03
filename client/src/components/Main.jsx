53d6d51cde1b785ff7ad1e753babd063e11fb735
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-08-03 16:27:38 +0300

    responsive banner on main page

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -3,7 +3,12 @@ import Login from './Login';
 import Layout from './Layout';
 import useAuthData from './useAuthData';
 import React from 'react';
-
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+import bannerLong from '../media/banner-long.png';
+import bannerShort from '../media/banner-short.webp'
 
 function Main() {
     const {
@@ -20,13 +25,13 @@ function Main() {
     return (
         <div className={'App' + (showLoginForm ? ' active' : '')}>
             {/* <div className='container'> */}
-                <Layout showDropdown={showDropdown}
-                    isAuthenticated={isAuthenticated}
-                    userEmail={userEmail}
-                    handleProfileIconClick={handleProfileIconClick}
-                    handleLogout={handleLogout}
-                    handleLoginLinkClick={handleLoginLinkClick}
-                />
+            <Layout showDropdown={showDropdown}
+                isAuthenticated={isAuthenticated}
+                userEmail={userEmail}
+                handleProfileIconClick={handleProfileIconClick}
+                handleLogout={handleLogout}
+                handleLoginLinkClick={handleLoginLinkClick}
+            />
             {/* </div > */}
             {showLoginForm && (
                 <div className='loginContainer' ref={loginContainerRef}>
@@ -34,11 +39,63 @@ function Main() {
                 </div>
             )
             }
-            {/* <div className='bannerContainer'>
-                <div className='bannerLong'></div>
-            </div> */}
+            <Box className='bannerContainer' sx={{
+                height: '40vw'
+            }}>
+                <Container sx={{
+                    display: { xs: 'none', sm: 'flex' }
+                }}>
+                    <img src={bannerLong} alt="" />
+                </Container>
+                <Container sx={{
+                    display: { xs: 'flex', sm: 'none' }
+                }}>
+                    <img src={bannerShort} alt="" />
+                </Container>
+
+            </Box>
+            <Container maxWidth="lg" sx={{
+                border: '2px solid #a6a6a6'
+            }}>
+                <Box sx={{ flexGrow: 1 }}>
+                    <div className='row1'>
+                        <h1>
+                            Підготуватися до складання іспиту на отримання водійського
+                            посвідчення дуже просто!
+                        </h1>
+                    </div>
+                    <Grid container spacing={{ xs: 2, md: 3 }} sx={{
+                        alignItems: 'center',
+                        display: { xs: 'none', md: 'flex' },
+                        justifyContent: 'center'
+                    }}>
+
+                        <div className='row2'>
+                            <div className='col1'>
+                                <span className='iconLearn'></span>
+                                <span>Вчися</span>
+                            </div>
+                            <div className='col1'>
+                                <span className='iconLearn'></span>
+                                <span>Проходь тести</span>
+                            </div>
+                            <div className='col1'>
+                                <span className='iconLearn'></span>
+                                <span>Складай іспит</span>
+                            </div>
+                        </div>
+                        <div className='row3'>
+                            <a href='start-testing'>Розпочати тестування</a>
+                            <a href='read-pdr'>Читати ПДР</a>
+                            <a href='start-learning'>Пройти навчання</a>
+                        </div>
+
+                    </Grid>
+                </Box>
+            </Container>
             <div className='container homepage'>
-                <div className='row'>
+
+                {/* <div className='row'>
                     <div className='row1'>
                         <h1>
                             Підготуватися до складання іспиту на отримання водійського
@@ -64,7 +121,7 @@ function Main() {
                         <a href='read-pdr'>Читати ПДР</a>
                         <a href='start-learning'>Пройти навчання</a>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div >
     );
