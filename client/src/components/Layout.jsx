f7e45530000ebc8cf6e66703177d8248ed559a53
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-08-04 11:53:51 +0300

    small layout changes

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -17,73 +17,7 @@ import MenuItem from '@mui/material/MenuItem';
 import MenuIcon from '@mui/icons-material/Menu';
 import LoginIcon from '@mui/icons-material/Login';
 
-const pages = ['Тести з ПДР', 'Іспит з водіння'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
-// function Layout(props) {
-//   const {
-//     showDropdown,
-//     isAuthenticated,
-//     userEmail,
-//     handleProfileIconClick,
-//     handleLogout,
-//     handleLoginLinkClick
-//   } = props;
-
-//   return (
-//     <header className='App-header'>
-//       <div className='headerContainer'>
-//         <a href='/'>
-//           <img src={logo} className='App-logo' alt='logo' />
-//         </a>
-//         <ul className='headerList'>
-//           <li>
-//             <a href='/sections'>Тести з ПДР</a>
-//           </li>
-//           <li>
-//             <a href='/exam'>Іспит з водіння</a>
-//           </li>
-//           <li className={showDropdown ? 'headerProfileActive' : ''}>
-//             {isAuthenticated ? (
-//               <div className="profileContainer">
-//                 <div className="action" onClick={handleProfileIconClick}>
-//                   <div className="profile">
-//                     <img src={profileIcon} alt="profile-img" />
-//                   </div>
-//                   <div className={`menu ${showDropdown ? 'active' : ''}`}>
-//                     <h3>
-//                       {userEmail}<br />
-//                       <span>Звичайний користувач</span>
-//                     </h3>
-//                     <ul>
-//                       <li>
-//                         <a href="#">Мій профіль</a>
-//                       </li>
-//                       <li>
-//                         <a href="/sections">Teсти</a>
-//                       </li>
-//                       <li>
-//                         <i className="far fa-envelope"></i>
-//                         <a onClick={handleLogout}>Вийти</a>
-//                       </li>
-//                     </ul>
-//                   </div>
-//                 </div>
-//               </div>
-//             ) : (
-//               <li>
-//                 <a onClick={handleLoginLinkClick}>
-//                   Особистий кабінет
-//                 </a>
-//               </li>
-
-//             )}
-//           </li>
-//         </ul>
-//       </div>
-//     </header>
-//   );
-// }
 
 function Layout(props) {
   const {
@@ -124,7 +58,7 @@ function Layout(props) {
             component="a"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
+              display: { xs: 'none', md: 'flex' }
             }}>
             <a href='/'>
               <img src={logo} className='App-logo' alt='logo' />
